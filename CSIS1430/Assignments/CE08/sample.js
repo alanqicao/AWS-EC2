7f6b44de763a19b0a99bbbb814ea88565c73d183
alanqicao <alanqicao@gmail.com>
2020-10-28 13:30:05 -0600

    Update

---
+++ CSIS1430/Assignments/CE08/sample.js
@@ -0,0 +1,48 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function firstFun(money) {
+    console.log(money);
+
+    var car;
+
+    var cost = money;
+
+    if (cost > 50000) {
+
+        car = "<header><h2><a href ='#'>BMW</a><h2><header>";
+
+    } else {
+
+        car = "Honda";
+
+    }
+
+    document.write(car);
+
+
+}
+
+function blastOff() {
+
+    for (I = 10; I > -1; I--) {
+
+        document.write(I + "<br>");
+
+    }
+
+    document.write("blast off!!!!");
+
+
+
+
+
+}
+
+//Qi Cao
+
+// 10-19-2020
